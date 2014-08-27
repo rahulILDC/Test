9e3f7939be7810cdd1fd24984521b7d1f0713e31
indianic <indianic@ind284.local>
2014-08-27 14:03:27 +0530

    Initial Commit

---
+++ Delete2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delete2
+//
+//  Created by indianic on 27/08/14.
+//  Copyright (c) 2014 IndiaNIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Delete2Tests/Delete2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Delete2Tests.m
+//  Delete2Tests
+//
+//  Created by indianic on 27/08/14.
+//  Copyright (c) 2014 IndiaNIC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Delete2Tests : XCTestCase
+
+@end
+
+@implementation Delete2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
